d8e2cac8e9dd14c9141834eed55488f231c3a048
Clay <cod5000@gmail.com>
2022-12-06 07:57:35 -0500

    Timeline updates

--- timeline.php
+++ timeline.php
@@ -16,18 +16,19 @@ include("includes/main_head.php");
       ?>
   
   <div id="timeline">
-    <ul id="dates" class="text-dark">
+    <ul id="dates" class="text-dark" style="margin-left:0">
     <?php for ($i = 0; $i < count($array); $i++): ?>
-      <li><a href="#<?php echo $array[$i]['start_date']['id'] ?>"><?php echo $array[$i]['start_date']['year'] ?></a></li>
+      <li class="<?php echo $array[$i]['group']?>"><a href="#<?php echo $array[$i]['start_date']['id'] ?>"><?php echo $array[$i]['start_date']['year'] ?></a></li>
     <?php endfor; ?>
     </ul>
 
     <ul id="issues">
       <?php for ($i = 0; $i < count($array); $i++): ?>
         <li id="<?php echo $array[$i]['start_date']['id'] ?>">
-          <h1><?php echo $array[$i]['start_date']['year'] ?></h1>
-          <div class="slider text-center">
-          <?php echo $array[$i]['text']['text'] ?>
+          <h1 style="padding: 0 40px"><?php echo $array[$i]['text']['headline'] ?></h1>
+          <p class="timeline-date" style="padding: 0 30px">- <?php echo $array[$i]['start_date']['month']." ".$array[$i]['start_date']['day'].", ".$array[$i]['start_date']['year']?> </p>
+          <div class="slider">
+          <p><?php echo $array[$i]['text']['text'] ?></p>
             </div>
         </li>
       <?php endfor; ?>
@@ -39,23 +40,8 @@ include("includes/main_head.php");
     <a href="#" id="next"><i class="bi-arrow-right"></i></a>
     <a href="#" id="prev">-</a>
   </div>
-  
-  <!-- <ul id="dates">
-    
-    <li><a href="#1900">1771</a></li>
-      <li hidden><a href="#1930">1793</a></li>
-      <li hidden><a href="#1944">1803</a></li>
-      <li hidden><a href="#1950">1804</a></li>
-      <li hidden><a href="#1971">1806</a></li>
-      <li hidden><a href="#1977">1808</a></li>
-      <li><a href="#1989">1989</a></li>
-      <li><a href="#1999">1999</a></li>
-      <li><a href="#2001">2001</a></li>
-      <li><a href="#2011">2011</a></li>
-      <li hidden><a href="#2011-2">2011</a></li>
-  </ul> -->
-
-  <ul id="dates" class="bottom-date" style="width:100%">
+ <div class="container">
+ <ul id="dates" class="bottom-date" style="width:100%; margin-left:0">
     <?php for ($i = 0; $i < count($array); $i++): ?>
       <?php $hidden = $array[$i]['start_date']['hidden'];
         if($array[$i]['start_date']['hidden'] == 'true'){
@@ -66,8 +52,17 @@ include("includes/main_head.php");
       ?>
       <li <?php echo $hidden ?>><a href="<?php echo $array[$i]['start_date']['id'] ?>"><?php echo $array[$i]['start_date']['year'] ?></a></li>
     <?php endfor; ?>
-    </ul>
-
+  </ul>
+ </div>
+<br>
+ <div class="container">
+ <ul id="dates" style="width:100%; margin-left:0">
+ <li  id="england-dates" style="margin-right:140px!important"><a href="">England</a></li>
+ <li id="northeast-dates" style="margin-right:140px!important"><a href="">Northeast</a></li>
+ <li id="southeast-dates" style="margin-right:140px!important"><a href="">Southeast</a></li>
+ <li id="caribbean-dates" style="margin-right:140px!important"><a href="">Caribbean</a></li>
+  </ul>
+ </div>
 
 <?php include("includes/main_js.php");?>
 <script src="js/jquery.timelinr-0.9.7.js"></script>
@@ -77,6 +72,20 @@ include("includes/main_head.php");
         arrowKeys: 'true'
       })
     });
+
+
+$("#england-dates" ).on( "click", function() {
+    $('.England').toggleClass( "highlight");
+});
+$("#northeast-dates" ).on( "click", function() {
+    $('.Northeast').toggleClass( "highlight");
+});
+$("#southeast-dates" ).on( "click", function() {
+    $('.Southeast').toggleClass( "highlight");
+});
+$("#caribbean-dates" ).on( "click", function() {
+    $('.Caribbean').toggleClass( "highlight");
+});
   </script>
 
 </body>
