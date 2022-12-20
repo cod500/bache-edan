dfa59afb81cc589f118386b5eade65fc6bf124b4
cod500 <cod5000@gmail.com>
2022-12-20 09:09:07 -0500

    Hide overflow

--- book-index.php
+++ book-index.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php 
-$title = "Ledger Book Index";
-include("includes/main_head.php");
-?>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <link rel="shortcut icon" href="https://npg.si.edu/favicon.ico" type="image/vnd.microsoft.icon" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Ledger Book Index</title>
+    <!-- CSS FILES -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/datatables.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="css/bache.css" rel="stylesheet">
+    <link href="css/modalzoom.css" rel="stylesheet">
+    <link href="css/npg.css" rel="stylesheet">
  
 </head>
-<body style="overflow: scroll; min-width: 720px;">
+<body style="overflow: scroll;">
 <?php include("includes/nav.php");?>
 <?php 
         $page = "Ledger Book Index";
@@ -120,7 +133,8 @@ include("includes/main_head.php");
 $(document).ready(function () {
     var datatable = $('table.display').DataTable({
         "pageLength": 25,
-        "order": [1, 'asc']
+        "order": [1, 'asc'],
+        responsive: true
     });
     
     //Call edan funciton on every table redraw to show modal
--- other-collections.php
+++ other-collections.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php 
-$title = "Silhouettes in Other Collections";
-include("includes/main_head.php");
-?>
- 
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <link rel="shortcut icon" href="https://npg.si.edu/favicon.ico" type="image/vnd.microsoft.icon" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Silhouettes in Other Collections</title>
+    <!-- CSS FILES -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/datatables.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="css/bache.css" rel="stylesheet">
+    <link href="css/modalzoom.css" rel="stylesheet">
+    <link href="css/npg.css" rel="stylesheet">
 </head>
 <body style="min-width: 720px;">
 <?php include("includes/nav.php");?>
